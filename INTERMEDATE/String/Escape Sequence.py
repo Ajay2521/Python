7cff9e824874da5af5babfa230037404f233c4bb
Ajay <60919132+maayon2521@users.noreply.github.com>
2020-07-04 10:59:31 +0530

    In this String concept has been explained.

---
+++ INTERMEDATE/String/Escape Sequence.py
@@ -0,0 +1,88 @@
+# In this lets see about "Escape Sequence" in Python.
+
+# The backslash(/) symbol denotes the escape sequence.
+
+#  The backslash can be followed by a special character.
+
+# Some of the Majorly used Escape Sequence are : 
+
+# \ - It ignore the new line.
+
+# \\ - prints a Backslash.
+
+# \' - Prints Single Quotes.
+
+# \" - Prints Double Quotes.
+
+# \a - Produce a bell sound.
+
+# \b - Produce a Backspace sound.
+
+# \f - Produce a formfeed.
+
+# \n - Produce a line feed or new line.
+
+# \r - Produce a Carriege Return.
+
+# \t - Produce a Horizontal Tab.
+
+# \v - Produce a Vertical Tab.
+
+# \ooo - Produce a Character with Octal Value.
+
+# \xHH - Produce a Character with Hexa Value.
+
+# Here is the program for "Escape Sequence".
+
+print ( )
+
+print( "Hello \
+Maayon" ) # Eg for - \
+
+print ( )
+
+print ( "\\" ) # Prints a Backslash.
+
+print ( )
+
+print ( " \' " ) # Prints a single quotes.
+
+print ( )
+
+print ( " \" " ) # Prints a double quotes.
+
+print ( )
+
+print ( " \a " ) # Produce a bell sound.
+
+print ( )
+
+print ( " Ajay\b " ) # Produce a Backspace which delete a character from right.
+
+print ( )
+
+print ( "Hello \f Maayon" ) # Produce a formfeed.
+
+print ( )
+
+print ( "Hello \nMaayon" ) # Produce a linefeed / new line.
+
+print ( )
+
+print ( "Hello \r Maayon" ) # Produce a Carriege Return.
+
+print ( )
+
+print ( "Hello \t Maayon" ) # Produce a Horizontal tab.
+
+print ( )
+
+print ( "Hello \v Maayon" ) # Produce a Vertical tab.
+
+print ( )
+
+print( "\110\145\154\154\157" ) # Produce a Octal  Value for the word Hello.
+
+print ( )
+
+print( "\x48\x65\x6c\x6c\x6f" ) # Produce a Hexa  Value for the word Hello.
