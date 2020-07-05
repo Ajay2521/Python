07f8399e819db26d45dc66744dd2b660716f614c
Ajay <60919132+maayon2521@users.noreply.github.com>
2020-07-05 10:40:43 +0530

    In this Tuple datatype in python has been explained.

---
+++ INTERMEDATE/Tuple/Built-in functions in Tuple.py
@@ -0,0 +1,25 @@
+# In this lets see about "Built-in functions for Tuple" in Python
+
+# Majorly used Built in functions in Tuple is :
+
+# 1) cmp ( Tuple_variable_1 , Tuple_variable_2 ) - used to compare two variables of Tuple datatype.
+
+# 2) len ( Tuple_variable ) - used to find the length of the Tuple.
+
+# 3) max ( Tuple_variable ) - used to find the maximum value in the Tuple.
+
+# 4) min ( Tuple_variable ) - used to find the minimum value in the Tuple.
+
+# 3) Tuple ( seq ) - used to converts any sequence to the Tuple.
+
+#Note - cmp() is no more in use after the version of python 3 and above
+
+# Here is the program for all Built in functions.
+
+Tuple = ( 30, 40, 50, 10, 20 )
+
+print ( "\nThe Length of the Tuple is : " , len ( Tuple ) ) 
+
+print ( "\nThe Maximum Value of the Tuple is : " , max ( Tuple ) ) 
+
+print ( "\nThe Minimum Value of the Tuple is : " , min ( Tuple ) ) 
