b486d3a7c164b8e421981f4507d0a85ed7d0a9a9
Ajay <60919132+maayon2521@users.noreply.github.com>
2020-07-07 08:37:47 +0530

    In this Dictionary datatype has been explained.

---
+++ INTERMEDATE/Dictionary/Printing keys in dict.py
@@ -0,0 +1,19 @@
+# In this lets see more about "Dictionary datatype" in python.
+
+# Dictionary is used to store the data in a key-value pair format.
+
+# In short it giveout the value for specific key.
+
+# A dictionary can be iterated using for loop.
+
+# Here is the program to print only the "keys of the dictionary"
+
+Details = { "Name" : "Maayon" , "Age" : 19 , "NickName" : "Techgen" }
+
+for x in Details:
+
+	print ( )
+
+	print ( x ) # Print only Keys in the dictionary.
+
+
---
+++ INTERMEDATE/Dictionary/Printing values in dict.py
@@ -0,0 +1,19 @@
+# In this lets see more about "Dictionary datatype" in python.
+
+# Dictionary is used to store the data in a key-value pair format.
+
+# In short it giveout the value for specific key.
+
+# A dictionary can be iterated using for loop.
+
+# Here is the program to print only the "Values of the dictionary"
+
+Details = { "Name" : "Maayon" , "Age" : 19 , "NickName" : "Techgen" }
+
+for x in Details:
+
+	print ( )
+
+	print ( Details[ x ] ) # Print only Values in the dictionary.
+
+
